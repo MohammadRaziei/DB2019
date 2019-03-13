e583a29cd01da7837015b7623ce82b6c79c4fdbd
mohammadraziei <mohammadraziei1375@gmail.com>
2019-03-14 00:07:48 +0330

    fixed some issues

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+//session_unset();
+?>
 <html>
 <head>
     <meta charset="UTF-8">
@@ -5,29 +8,48 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <script>
+    </script>
 </head>
+<!--<body onreload="window.location = window.location.pathname;">-->
 <body>
+<?php
+session_start();
+//$_session['filename'] = "10";
+echo '<p>'.$_session['filename'].'</p>';
+$default_file = "test.sql";
+if(!isset($_session['filename']))
+    $_session['filename'] = $default_file;
 
 
+?>
 <strong>Test Form</strong>
 <form action="" method="post">
-<input type="text" name="picturenum"/>
-<input type="submit" name="Submit" value="Submit!" />
+<input type="text" value="<?php echo($_session['filename']) /*or echo($default_file)*/ ;?>" name="filename"/>
+<input type="submit" name="Submit" value="run" />
 </form>
 
 
 <?php
 
-session_start();
+
 //$_session['picturenum'] = 'test';
 //$test = $_POST['submit'];
 if (isset($_POST['Submit'])) {
-    $_session['filename'] = $_POST['picturenum'];
+    $_session['filename'] = $_POST['filename'];
+    $submit = true;
+}
+else{
+    $submit = false;
 }
 
-echo "<strong>". $_session['filename']."</strong><br/>";?>
+echo "<strong>".$submit."<br/>". $_session['filename']."</strong><br/>";?>
 
 </body>
+<script>
+    // window.location = window.location.href.split("?")[0];
+
+</script>
 </html>
 <?php
 //session_start();
@@ -41,6 +63,7 @@ $_session['mysql_username'] = 'root';
 $_session['mysql_password'] = '';
 // Database name
 $_session['mysql_database'] = 'db2019';
-include('run_sql.php');
-
+if($submit) {
+    include('run_sql.php');
+}
 ?>
