8faf679c96c1a3a70f45eb47e046827a3c1502e9
mohammadraziei <mohammadraziei1375@gmail.com>
2019-03-23 16:36:06 +0430

    fixed some issues in clean.php

--- clean.php
+++ clean.php
@@ -1,11 +1,28 @@
 <?php
-$conn = mysqli_connect("localhost","root","","db2019" );
+
+/*
+$filename = $_session['filename'];
+// MySQL host
+$mysql_host = $_session['mysql_host'];
+// MySQL username
+$mysql_username = $_session['mysql_username'];
+// MySQL password
+$mysql_password = $_session['mysql_password'];
+// Database name
+$mysql_database = $_session['mysql_database'];
+
+
+$conn = @new mysqli($mysql_host,$mysql_username,$mysql_password);
+*/
+$mysql_database = "db2019";
+$conn = mysqli_connect("localhost","root","");
 // Check connection
 if ($conn->connect_errno) {
     echo "Failed to connect to MySQL: " . $conn->connect_errno;
     echo "<br/>Error: " . $conn->connect_error;
 }
-
+$conn->query('CREATE DATABASE IF NOT EXISTS '.$mysql_database.';'); // for first run
+mysqli_select_db ( $conn , $mysql_database );
 $conn->query('DROP DATABASE db2019;') or print('Error performing query \'<strong>' .'DROP DATABASE db2019;' . '\': ' . $conn->error() . '<br /><br />');
 $conn->query('CREATE DATABASE db2019;') or print('Error performing query \'<strong>' . 'CREATE DATABASE db2019;' . '\': ' . $conn->error() . '<br /><br />');
 //$conn->query('USE DATABASE db2019;') or print('Error performing query \'<strong>' . 'USE DATABASE db2019;' . '\': ' . $conn->error() . '<br /><br />');
